249c99a830117f2c9a4c6476673e27e5f674205d
alek <ali.hatbi@wp.pl>
2023-05-16 10:15:29 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<RownanieKwadratowe> Delty = new ArrayList<>();
+
+        Delty.add(new RownanieKwadratowe(scanner.nextInt(),scanner.nextInt(),scanner.nextInt()));
+        Delty.add(new RownanieKwadratowe(scanner.nextInt(),scanner.nextInt(),scanner.nextInt()));
+        Delty.add(new RownanieKwadratowe(scanner.nextInt(),scanner.nextInt(),scanner.nextInt()));
+        Delty.add(new RownanieKwadratowe(scanner.nextInt(),scanner.nextInt(),scanner.nextInt()));
+
+        System.out.println(Delty.get(0).delta());
+        System.out.println(Delty.get(1).delta());
+        System.out.println(Delty.get(2).delta());
+        System.out.println(Delty.get(3).delta());
+
     }
 }
--- src/RownanieKwadratowe.java
+++ src/RownanieKwadratowe.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class RownanieKwadratowe {
+import java.util.Objects;
+
+import static java.lang.Math.sqrt;
+
+/**
+ RownanieKwadratowe - Klasa wykonująca równania kwadratowe
+ a,b,c - pola liczbowe do wykonania równań
+
+ @author: Erwin,Łukasik
+
+ **/
+
+public class RownanieKwadratowe {
+    private final double a,b,c;
+
+    public RownanieKwadratowe(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Double delta(){
+        double x=b*b-(4*a*c);
+        if(x<0){
+            return 0.0;
+        }if(x==0){
+            return b*-1/2*a;
+        }else{
+            return b*-1+sqrt(x)/2*a + b*-1-sqrt(x)/2*a;
+        }
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RownanieKwadratowe that)) return false;
+        return a == that.a && b == that.b && c == that.c;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b, c);
+    }
+
+    @Override
+    public String toString() {
+        return
+                a +
+                "x^2 + " + b +
+                "x + " + c
+                ;
+    }
 }
